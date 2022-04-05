0ca30e2ff17f92e8c9a15316cb6bb6a80043a844
EdenGenetTasama <edentasama19@gmail.com>
2022-04-05 18:13:02 +0300

    add change

--- server/controllers/User-Cntrl.js
+++ server/controllers/User-Cntrl.js
@@ -1,40 +1,7 @@
 const Users = require("../model/User-Model");
 
 module.exports ={
-    getAllUsers: (req, res) => {
-        try{
-            Users.find((err, users) => {
-                if(err){
-                    res.status(404).json({message: 'Error getting users'})
-                }
-                console.log("Get all users")
-                res.status(200).json(users)
-            })
-        }
-        catch(err){
-            res.status(500).json({message: 'Error getting users'})
-        }
-    },
-    getUserId: (req, res) => {
-        try{
 
-        }
-        catch(err){
-            
-        }
-    },
-    postUser: async(req, res) => {
-        try{
-            await Users.create(req.body).then(() =>
-                res.status(200).json({ message: 'Post added successfully' })
-              )
-              .catch(err => res.status(500).json("err"))
-            }
-        catch(err){
-            res.status(500).json({message: 'Error post users'})
-            console.log(err);
-        }
-    },
     putUserId: (req, res) => {
         try{
 
