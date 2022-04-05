b02cccd1e51785309f9ceeb8e6a6941402e0a09b
EdenGenetTasama <edentasama19@gmail.com>
2022-04-06 01:18:44 +0300

    add one function

--- server/controllers/User-Cntrl.js
+++ server/controllers/User-Cntrl.js
@@ -1,26 +1,25 @@
-const Users = require("../model/User-Model");
-const Jobs = require("../model/Job-Model");
+const Users = require('../model/User-Model')
+const Jobs = require('../model/Job-Model')
 
-module.exports ={
+module.exports = {
+  putUserId: async (req, res) => {
+    try {
+      await Users.findByIdAndUpdate(req.params.id, req.body)
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(404).json(err))
+    } catch (err) {
+      res.status(500).json(err)
+    }},
 
-    putUserId: (req, res) => {
-        try{
-
-        }
-        catch(err){
-            
-        }
-    },
-    deleteUserId:async (req, res) => {
-        try{
-         const deleteUser = await Users.findByIdAndRemove({_id : req.params.id})
-         Jobs.findOne({userId : deleteUser._id}).then()
-        }
-        catch(err){
-            
-        }
-    },
+  deleteUserId: async (req, res) => {
+    try {
+      const deleteUser = await Users.findByIdAndRemove({ _id: req.params.id }) .then(data =>{ res.status(200).json(data); `http://localhost:5000/job/${deleteUser._id}`})
+            .catch(err => res.status(404).json(err))
+      console.log(deleteUser)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  }
 }
 
 // spec
-
