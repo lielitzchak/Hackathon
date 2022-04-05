835e186793f99acc4d0b093709ecff3c33a67a6a
EdenGenetTasama <edentasama19@gmail.com>
2022-04-05 19:01:26 +0300

    add function

--- server/controllers/Jobs-Cntrl.js
+++ server/controllers/Jobs-Cntrl.js
@@ -1,54 +1,60 @@
-const jobs = require("../model/Job-Model");
+const jobs = require('../model/Job-Model')
+const Users = require('../model/User-Model')
+
 module.exports = {
   getAllJobs: async (req, res) => {
     try {
       await jobs
         .find()
-        .then((data) => res.status(200).json(data))
-        .catch((err) => res.status(404).json(err));
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(404).json(err))
     } catch (err) {
-      res.status(500).json({ massage: "Server Error" });
+      res.status(500).json({ massage: 'Server Error' })
     }
   },
   getJobId: (req, res) => {
     try {
       jobs
         .findById(req.params.id)
-        .then((data) => res.status(200).json(data))
-        .catch((err) => res.status(404).json(err));
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(404).json(err))
     } catch (err) {
-      res.status(500).json({ massage: "Server Error" });
+      res.status(500).json({ massage: 'Server Error' })
     }
   },
   postJob: (req, res) => {
     try {
-      jobs
-        .findOne(req.body.userId)
-        .populate("userId")
-        .then((data) => res.status(200).json(data))
-        .catch((err) => res.status(404).json(err));
+      const jobFound = jobs
+        .create(req.body)
+        .then(data => {
+          res.status(200).json(data)
+          Users.findById(req.body.userId).then(() =>
+            Users.updateOne({ $push: { jobs: data._id } })
+          )
+        })
+        .catch(err => console.error(err))
     } catch (err) {
-      res.status(500).json({ massage: "Server Error" });
+      res.status(500).json({ massage: 'Server Error' })
     }
   },
   putJobId: (req, res) => {
     try {
       jobs
         .findByIdAndUpdate(req.params.id, req.body)
-        .then((data) => res.status(200).json(data))
-        .catch((err) => res.status(404).json(err));
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(404).json(err))
     } catch (err) {
-      res.status(500).json({ massage: "Server Error" });
+      res.status(500).json({ massage: 'Server Error' })
     }
   },
   deleteJobId: (req, res) => {
     try {
       jobs
         .findByIdAndDelete(req.params.id)
-        .then((data) => res.status(200).json(data))
-        .catch((err) => res.status(404).json(err));
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(404).json(err))
     } catch (err) {
-      res.status(500).json({ massage: "Server Error" });
+      res.status(500).json({ massage: 'Server Error' })
     }
-  },
-};
+  }
+}
