68bbf4d10c8ab96f7898cb5cf2c3bb3764cf8546
EdenGenetTasama <edentasama19@gmail.com>
2022-04-06 03:24:43 +0300

    add

--- client/src/AppRouter.jsx
+++ client/src/AppRouter.jsx
@@ -3,6 +3,7 @@ import JobsCalender from "./components/pages/calender/Calender";
 import Graph from "./components/pages/graph/Graph";
 import LoginRegister from "./components/pages/login-register/Login-register";
 import Header from "./components/parts/header/header";
+import Home from "./components/pages/home/home";
 import Footer from "./components/parts/footer/Footer";
 import { useContext } from "react";
 import { UserContext } from "./contexts/UserContext";
@@ -18,9 +19,9 @@ const AppRouter = () => {
         </>
       }
       <Routes>
-        <Route exact path="/" element={<LoginRegister />} />
-        <Route exact path="/calender" element={<JobsCalender />} />
-        <Route exact path="/graph" element={<Graph />} />
+        <Route exact path="/" element={user?<Home/>:<LoginRegister />} />
+        <Route exact path="/calender" element={user?<JobsCalender />:<LoginRegister />} />
+        <Route exact path="/graph" element={user?<Graph/>:<LoginRegister />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/components/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+ const Home=()=> {
+  return (
+    <div>home
+        
+    </div>
+  )
+}
+export default Home 
